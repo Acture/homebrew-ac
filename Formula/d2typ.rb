f848d684e26e6f3a7444d18018c148905f9278ec
acture <acturea@gmail.com>
2025-08-09 00:58:55 +0800

    feat(formula): add d2typ Homebrew formula
    Introduce a Homebrew formula for the `d2typ` CLI tool. Supports installation from source using Cargo and includes version livecheck for automated updates. Adds a basic test case to verify installation.

---
+++ Formula/d2typ.rb
@@ -0,0 +1,23 @@
+class D2typ < Formula
+  desc "🔁 Convert structured data (CSV/JSON/YAML/TOML/XLSX) into Typst syntax for embedding in documents."
+  homepage "https://github.com/Acture/d2typ"
+  url "https://github.com/Acture/d2typ/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "800113dcc8eb320557ce22ebbf69729059356a265c5f0f95515bae7193b2860a"
+  license ""
+
+  depends_on "rust" => :build
+
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"d2typ", "--version"
+  end
+
+  livecheck do
+      url :stable
+      regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+end
